e1b6dad832fda96850234ad2d965919b8b1dcc17
Gastgebruiker <Guest@fullname.local>
2025-05-06 12:10:24 +0200

    pdate

--- PlantPal/ContentView.swift
+++ PlantPal/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Text("gfklnblofh")
+                .padding(100.0)
         }
-        .padding()
     }
 }
 
